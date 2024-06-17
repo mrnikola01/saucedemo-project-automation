f5c9f37e1ac056b61a5a5435aac133cbc242d610
mrnikola01 <vukosavljevicn.va@gmail.com>
2024-06-17 19:55:34 +0200

    Add CheckoutOverview Test and methods in Cart Page

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CartPage extends BaseTest {
@@ -24,6 +25,31 @@ public class CartPage extends BaseTest {
     public WebElement continueShoppingButton;
 
     //-------------------------------------------------------------------------
+
+    public List<String> getALlProductsPrices() {
+        List<String> productsPrices = new ArrayList<>();
+
+        for(WebElement product : products) {
+            productsPrices.add(getProductPrice(product));
+        }
+
+        return productsPrices;
+    }
+
+    public double sumAllProductPrices(List<String> prices) {
+        double sum = 0;
+
+        for(String price : prices) {
+            double currentPrice = Double.parseDouble(price.replace("$", ""));
+            sum += currentPrice;
+        }
+
+        return sum;
+    }
+
+    public double calculateTax(double price) {
+        return (double) Math.round((price * 0.08) * 100) / 100;
+    }
 
     public String getProductName(WebElement product) {
         return product.findElement(By.className("inventory_item_name")).getText();
---
+++ src/test/java/tests/CheckoutOverviewTest.java
@@ -0,0 +1,50 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class CheckoutOverviewTest extends BaseTest {
+
+    @BeforeMethod
+    public void pageSetUp() {
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
+        loginUserWithCredentials("standard_user", "secret_sauce");
+        resetAppState();
+
+        inventoryPage.addProductInCart("Sauce Labs Bolt T-Shirt");
+        inventoryPage.addProductInCart("Sauce Labs Onesie");
+        navbarPage.clickOnShoppingCart();
+        cartPage.clickOnCheckoutButton();
+        fillCheckoutForm("Petar", "Petrovic", "11000");
+    }
+
+    @Test
+    public void itemTotalPriceIsCorrect() {
+        List<String> productsPrices = cartPage.getALlProductsPrices();
+
+        Assert.assertEquals(cartPage.sumAllProductPrices(productsPrices), 23.98);
+    }
+
+    @Test
+    public void taxIsCorrect() {
+        List<String> productsPrices = cartPage.getALlProductsPrices();
+        double priceOfProducts = cartPage.sumAllProductPrices(productsPrices);
+
+        Assert.assertEquals(cartPage.calculateTax(priceOfProducts), 1.92);
+    }
+
+    @Test
+    public void totalPriceIsCorrect() {
+        List<String> productsPrices = cartPage.getALlProductsPrices();
+        double priceOfProducts = cartPage.sumAllProductPrices(productsPrices);
+        double tax = cartPage.calculateTax(priceOfProducts);
+
+        Assert.assertEquals((priceOfProducts + tax), 25.90);
+    }
+}
